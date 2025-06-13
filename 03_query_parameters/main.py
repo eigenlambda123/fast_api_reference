b70cde0affbf8d0e88f8feb6c2d27a465817ec53
eigenlambda123 <rmvilla987@gmail.com>
2025-06-13 11:25:47 +0800

    query parameters

---
+++ 03_query_parameters/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}] # Example database
+
+# This example shows how to use query parameters in FastAPI.
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
+
+# This example shows how to use query parameters with optional parameters in FastAPI.
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+# This example shows how to use multiple path parameters with query parameters in FastAPI.
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
