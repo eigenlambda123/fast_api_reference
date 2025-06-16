a80c018ecf6d5e356add7ab034a0bf118e8ad4ba
eigenlambda123 <rmvilla987@gmail.com>
2025-06-16 09:15:18 +0800

    10b_list_fields_with_type_parameters

---
+++ 10_body_nested_models/10b_list_fields_with_type_parameters.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: list[str] = [] # List of tags for the item with type parameter
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
+    return results
