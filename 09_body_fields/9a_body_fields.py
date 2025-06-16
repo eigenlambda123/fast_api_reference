b69d89a4b4e1bfc024063c5b2095365b306e5f07
eigenlambda123 <rmvilla987@gmail.com>
2025-06-16 09:01:34 +0800

    9a_body_fields

---
+++ 09_body_fields/9a_body_fields.py
@@ -0,0 +1,21 @@
+from typing import Annotated
+
+from fastapi import Body, FastAPI
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = Field(
+        default=None, title="The description of the item", max_length=300
+    ) # use Field to add metadata and validation
+    price: float = Field(gt=0, description="The price must be greater than zero") # use Field to add validation
+    tax: float | None = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
+    results = {"item_id": item_id, "item": item}
+    return results
