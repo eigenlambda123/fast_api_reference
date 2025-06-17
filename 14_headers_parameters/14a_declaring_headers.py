8de71b72e1a83eab3d657f9c4d9d0a94ac31780e
eigenlambda123 <rmvilla987@gmail.com>
2025-06-17 14:52:14 +0800

    14a_declaring_headers

---
+++ 14_headers_parameters/14a_declaring_headers.py
@@ -0,0 +1,10 @@
+from typing import Annotated
+
+from fastapi import FastAPI, Header
+
+app = FastAPI()
+
+
+@app.get("/items/")
+async def read_items(user_agent: Annotated[str | None, Header()] = None): # Read User-Agent from headers
+    return {"User-Agent": user_agent}
