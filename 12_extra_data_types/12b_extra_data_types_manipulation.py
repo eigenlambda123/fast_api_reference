dae20459e11172ace36f0b100cc12574515371be
eigenlambda123 <rmvilla987@gmail.com>
2025-06-17 14:43:42 +0800

    12b_extra_data_types_manipulation

---
+++ 12_extra_data_types/12b_extra_data_types_manipulation.py
@@ -0,0 +1,31 @@
+from datetime import datetime, time, timedelta
+from typing import Annotated
+from uuid import UUID
+
+from fastapi import Body, FastAPI
+
+app = FastAPI()
+
+
+@app.put("/items/{item_id}")
+async def read_items(
+    item_id: UUID,
+    start_datetime: Annotated[datetime, Body()],
+    end_datetime: Annotated[datetime, Body()],
+    process_after: Annotated[timedelta, Body()],
+    repeat_at: Annotated[time | None, Body()] = None,
+):
+    
+    # Manipulate extra data types
+    start_process = start_datetime + process_after # Calculate when the process should start
+    duration = end_datetime - start_process # Calculate the duration of the process
+
+    return {
+        "item_id": item_id,
+        "start_datetime": start_datetime,
+        "end_datetime": end_datetime,
+        "process_after": process_after,
+        "repeat_at": repeat_at,
+        "start_process": start_process,
+        "duration": duration,
+    }
