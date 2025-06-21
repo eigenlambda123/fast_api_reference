69dfdea90cd7c9699c33aa6aea7a27b6c32c600b
eigenlambda123 <rmvilla987@gmail.com>
2025-06-21 09:44:01 +0800

    20a_declaring_form_data

---
+++ 20_form_data/20a_declaring_form_data.py
@@ -0,0 +1,10 @@
+from typing import Annotated
+
+from fastapi import FastAPI, Form
+
+app = FastAPI()
+
+
+@app.post("/login/")
+async def login(username: Annotated[str, Form()], password: Annotated[str, Form()]): # Use Form() to declare form data
+    return {"username": username}
