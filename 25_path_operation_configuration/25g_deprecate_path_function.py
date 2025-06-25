23b93adad402dac4bf6da2ea7f9dbae912ab1993
eigenlambda123 <rmvilla987@gmail.com>
2025-06-25 11:29:19 +0800

    25g_deprecate_path_function

---
+++ 25_path_operation_configuration/25g_deprecate_path_function.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/items/", tags=["items"])
+async def read_items():
+    return [{"name": "Foo", "price": 42}]
+
+
+@app.get("/users/", tags=["users"])
+async def read_users():
+    return [{"username": "johndoe"}]
+
+
+@app.get("/elements/", tags=["items"], deprecated=True) # Mark this path operation as deprecated
+async def read_elements():
+    return [{"item_id": "Foo"}]
